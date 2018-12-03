c02d8c682d54c44a7a1bfaf97ec1df36bb700c33
Kaustubh Garimella <kaustubh.garimella@gmail.com>
2018-12-02 19:15:46 -0800

    combat engine

--- Assets/Scripts/Combat/CombatEngine.cs
+++ Assets/Scripts/Combat/CombatEngine.cs
@@ -1,3 +1,4 @@
+using System;
 using ManaMist.Combat;
 using ManaMist.Utility;
 
@@ -7,21 +8,33 @@ namespace ManaMist.Combat
     {
         public CombatResult Battle(CombatEntity attacker, CombatEntity defender)
         {
-            defender.hp -= ManaMistMath.Clamp(attacker.attack - defender.defense, 0, defender.hp);
-
-            if (defender.hp <= 0)
+            if (WillHit(attacker, defender))
             {
-                return CombatResult.WIN;
+                defender.hp -= ManaMistMath.Clamp(attacker.attack - defender.defense, 0, defender.hp);
+
+                if (defender.hp <= 0)
+                {
+                    return CombatResult.WIN;
+                }
             }
 
-            attacker.hp -= ManaMistMath.Clamp(defender.attack - attacker.defense, 0, attacker.hp);
-
-            if (attacker.hp <= 0)
+            if (WillHit(defender, attacker))
             {
-                return CombatResult.LOSS;
+                attacker.hp -= ManaMistMath.Clamp(defender.attack - attacker.defense, 0, attacker.hp);
+
+                if (attacker.hp <= 0)
+                {
+                    return CombatResult.LOSS;
+                }
             }
 
             return CombatResult.NONE;
+        }
+
+        public bool WillHit(CombatEntity attacker, CombatEntity defender)
+        {
+            int hitChance = attacker.skill - defender.speed;
+            return new Random().Next(0, 100) > hitChance;
         }
     }
 }
--- Assets/Scripts/Combat/CombatEntity.cs
+++ Assets/Scripts/Combat/CombatEntity.cs
@@ -7,6 +7,8 @@ namespace ManaMist.Combat
         public int hp { get; set; }
         public int attack { get; set; }
         public int defense { get; set; }
+        public int skill { get; set; }
+        public int speed { get; set; }
         public CombatEntity(string name, Cost cost) : base(name, cost)
         {
         }
