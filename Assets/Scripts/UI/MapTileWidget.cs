a8eaf02b8149c8472fda7d06e1fc1f5fdd5b1d9f
Kaustubh Garimella <kaustubh.garimella@gmail.com>
2018-12-02 22:35:42 -0800

    map colors working

--- Assets/Scripts/UI/MapTileWidget.cs
+++ Assets/Scripts/UI/MapTileWidget.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MapTileWidget : MonoBehaviour {
+public class MapTileWidget : MonoBehaviour
+{
 
     public MapTile mapTile = null;
 
@@ -31,35 +32,35 @@ public class MapTileWidget : MonoBehaviour {
         {
             case ManaMist.Models.Terrain.DESERT:
 
-                return new Color(255, 214, 127);
+                return new Color32(255, 214, 127, 255);
 
             case ManaMist.Models.Terrain.FOREST:
 
-                return new Color(8, 89, 0);
+                return new Color32(8, 89, 0, 255);
 
             case ManaMist.Models.Terrain.GRASS:
 
-                return new Color(108, 226, 95);
+                return new Color32(108, 226, 95, 255);
 
             case ManaMist.Models.Terrain.HILL:
 
-                return new Color(21, 214, 0);
+                return new Color32(21, 214, 0, 255);
 
             case ManaMist.Models.Terrain.MOUNTAIN:
 
-                return new Color(91, 91, 91);
+                return new Color32(91, 91, 91, 255);
 
             case ManaMist.Models.Terrain.SWAMP:
 
-                return new Color(69, 91, 18);
+                return new Color32(69, 91, 18, 255);
 
             case ManaMist.Models.Terrain.WATER:
 
-                return new Color(66, 212, 244);
+                return new Color32(66, 212, 244, 255);
 
             default:
 
-                return new Color(0, 0, 0);
+                return new Color32(0, 0, 0, 255);
 
         }
     }
