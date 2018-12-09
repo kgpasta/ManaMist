a9987056c168d3c919572346c096607cb16010a5
prashant <prashant@pixvana.com>
2018-12-09 15:18:49 -0800

    adding fog of war enum
    - TODO QUESTION
    should the map know which player has visited each tile and what it's fogofwar state is for each player OR should each player have it's own Dictionary<Coordinate, FogOfWar>?
    
    former might make more sense architecturally, latter seems easier to implement...unsure

---
+++ Assets/Scripts/Models/FogOfWar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ManaMist.Models
+{
+    public enum FogOfWar
+    {
+        // A player's unit has never seen or visited
+        // Visual: can only see the tile terrain
+        INVISIBLE,
+        // A player's unit has visited but currently can't see
+        // Visual: can see the terrain, resource, and last seen entity on that tile(?)
+        FOGGY,
+        // A player's unit has visited and currently can see
+        // Visual: can see all the tile's data
+        VISIBLE
+    }
+}
+
