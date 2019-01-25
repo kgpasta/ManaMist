77762e66d3d84dc4cacb6338a53ddeabf705e53c
prashant-naidu <naidu.s.prashant@gmail.com>
2019-01-24 22:49:13 -0800

    adding inspector canvas prefab

---
+++ Assets/Scripts/TemporaryUI/EntityInspectorCanvas.cs
@@ -0,0 +1,24 @@
+﻿using ManaMist.Models;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class EntityInspectorCanvas : MonoBehaviour
+{
+    public Entity entity = null;
+
+    [Header("UI Elements")]
+    [SerializeField] private TextMeshProUGUI m_NameText = null;
+    [SerializeField] private TextMeshProUGUI m_TypeText = null;
+
+    private void OnGUI()
+    {
+        if (entity != null)
+        {
+            m_NameText.text = entity.name;
+            m_TypeText.text = entity.type.ToString();
+        }
+    }
+}
