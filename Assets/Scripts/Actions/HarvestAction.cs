a8db414f3457732bc78e5202c5952fca761e64a1
Kaustubh Garimella <kaustubh.garimella@gmail.com>
2019-01-26 16:09:23 -0800

    turn start actions

--- Assets/Scripts/Actions/HarvestAction.cs
+++ Assets/Scripts/Actions/HarvestAction.cs
@@ -1,14 +1,23 @@
 using ManaMist.Models;
+using ManaMist.Players;
 using ManaMist.Utility;
 using UnityEngine;
 
 namespace ManaMist.Actions
 {
     [CreateAssetMenu(menuName = "ManaMist/Actions/HarvestAction")]
-    public class HarvestAction : Action, IBuildConstraint
+    public class HarvestAction : Action, IBuildConstraint, ITurnStartAction
     {
         public Resource resource;
         public Cost harvestAmount;
+
+        [SerializeField] private int m_TurnLeft = 100000; //Essentially infinite, since this never expires
+        public int TurnsLeft { get => m_TurnLeft; set => m_TurnLeft = value; }
+
+        public override void Execute(Player player, Entity entity, Coordinate targetCoordinate = null, Entity target = null)
+        {
+            player.resources.Increment(harvestAmount);
+        }
 
         public bool CanBuild(MapTile mapTile)
         {
