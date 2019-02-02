9b169072e03314b0d6a7693e277c6799f6d9872f
Kaustubh Garimella <kaustubh.garimella@gmail.com>
2019-02-02 15:06:39 -0800

    change color when action points are depleted

--- Assets/Scripts/UI/EntityView.cs
+++ Assets/Scripts/UI/EntityView.cs
@@ -6,12 +6,18 @@ namespace ManaMist.UI
     public class EntityView : MonoBehaviour
     {
         public Entity entity;
+        private Color baseColor;
 
         public void Initialize(Entity entity)
         {
             this.entity = entity;
             this.gameObject.name = entity.name;
-            this.GetComponent<MeshRenderer>().material.color = entity.color;
+            this.baseColor = entity.color;
+        }
+
+        private void OnGUI()
+        {
+            this.GetComponent<MeshRenderer>().material.color = entity.actionPoints > 0 ? baseColor : Color.gray;
         }
     }
 }
