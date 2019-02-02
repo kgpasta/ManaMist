9bc93e94ae84bb499aa2b246371d5f633393a28c
Kaustubh Garimella <kaustubh.garimella@gmail.com>
2019-02-02 15:08:13 -0800

    rename private variable

--- Assets/Scripts/UI/EntityView.cs
+++ Assets/Scripts/UI/EntityView.cs
@@ -6,18 +6,18 @@ namespace ManaMist.UI
     public class EntityView : MonoBehaviour
     {
         public Entity entity;
-        private Color baseColor;
+        private Color m_BaseColor;
 
         public void Initialize(Entity entity)
         {
             this.entity = entity;
             this.gameObject.name = entity.name;
-            this.baseColor = entity.color;
+            this.m_BaseColor = entity.color;
         }
 
         private void OnGUI()
         {
-            this.GetComponent<MeshRenderer>().material.color = entity.actionPoints > 0 ? baseColor : Color.gray;
+            this.GetComponent<MeshRenderer>().material.color = entity.actionPoints > 0 ? m_BaseColor : Color.gray;
         }
     }
 }
